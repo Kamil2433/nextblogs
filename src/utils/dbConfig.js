20cc730aa3f1b2c87118c4481dcef58493c4b68f
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-11 19:52:59 +0530

    Update dbConfig.js

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -9,7 +9,7 @@ const options = {
 let mongoClient = null;
 let database = null;
 
-if (!process.env.MONGO_URI) {
+if (!process.env.MONGODB_URI) {
     throw new Error('Please add your Mongo URI to .env.local')
 }
 
@@ -28,7 +28,7 @@ export async function connect() {
         } else {
             mongoClient = await (new MongoClient(uri, options)).connect();
         }
-        database = await mongoClient.db(process.env.MONGO_URI);
+        database = await mongoClient.db(process.env.MONGODB_URI);
         return { mongoClient, database };
     } catch (e) {
         console.error(e);
