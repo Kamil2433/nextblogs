f365b258c4598764c8eed257fc2c304464d6ebd1
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-11 14:53:42 +0530

    Update page.jsx

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from "./page.module.css";
 import Link from 'next/link';
-import { BASE_URL } from '@/utils/Constants';
+
 
 const getData = async()=>{
   const res = await fetch(`${process.env.DEV_URL}/api/posts/all`)
