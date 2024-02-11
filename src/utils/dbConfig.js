c9056afb0f8af7e3a5a069b3baed0ea7f52c19b1
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-11 19:58:50 +0530

    Update dbConfig.js

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -1,36 +1,31 @@
-import { MongoClient } from "mongodb";
+const mongoose=require('mongoose')
+const mongoURI=process.env.MONGODB_URI
 
-const uri = process.env.MONGODB_URI
-const options = {
-    useUnifiedTopology: true,
+// 'mongodb://127.0.0.1:27017/CloudNote1?directConnection=true'
+// mongodb://localhost:27017/
+// mongodb://127.0.0.1:27017/?directConnection=true
+
+
+const connectionParams={
     useNewUrlParser: true,
-};
-
-let mongoClient = null;
-let database = null;
-
-if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your Mongo URI to .env.local')
+      useUnifiedTopology: true 
 }
 
-export async function connect() {
-    try {
-        if (mongoClient && database) {
-            return { mongoClient, database };
-        }
-        if (process.env.NODE_ENV === "development") {
-            if (!global._mongoClient) {
-                mongoClient = await (new MongoClient(uri, options)).connect();
-                global._mongoClient = mongoClient;
-            } else {
-                mongoClient = global._mongoClient;
-            }
-        } else {
-            mongoClient = await (new MongoClient(uri, options)).connect();
-        }
-        database = await mongoClient.db(process.env.MONGODB_URI);
-        return { mongoClient, database };
-    } catch (e) {
-        console.error(e);
-    }
+const connectmongo=async()=>{
+
+// const mon=await mongoose.connect(mongoURI,connectionParams)
+
+// console.log("connected successfully to mongodb")
+
+mongoose.connect(mongoURI,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
 }
+
+
+module.exports=connectmongo;
