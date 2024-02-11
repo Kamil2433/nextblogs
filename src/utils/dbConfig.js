ea7879d4ffd5a92d156f498ce2346018263c9537
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-11 10:44:34 +0530

    Update dbConfig.js

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -1,29 +1,49 @@
-import { MongoClient } from 'mongodb'
 
-const uri = process.env.Mongo
-const options = {}
 
-let client
-let clientPromise
 
-if (!process.env.Mongo) {
-  throw new Error('Please add your Mongo URI to .env.local')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import mongoose from "mongoose";
+
+export const connect = async () => {
+
+     
+    
+
+    try {
+        mongoose.connect(process.env.Mongo);
+        const connection = mongoose.connection;
+        connection.on('connected', () => {
+            console.log('MongoDB connected');
+        })
+        connection.on('error', (error) => {
+            console.log('MongoDB connection error.');
+            process.exit();
+        })
+    } catch (error) {
+        console.log("something went wrong");
+        console.log(error);
+    }
+
 }
-
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-}
-
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise
