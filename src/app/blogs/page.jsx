cd8579a36e22793a01f9d665a287250d67a3876f
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-11 17:40:47 +0530

    Update page.jsx

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable */
-
+"use client";
 import React from 'react';
 import styles from "./page.module.css";
 import Link from 'next/link';
+import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
+import useSWR from "swr";
 
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 const getData = async()=>{
   const res = await fetch(`${process.env.DEV_URL}/api/posts/all`)
@@ -17,8 +21,10 @@ const getData = async()=>{
   return res.json();
 }
 
-const Blogs = async() => {
-  const data = await getData();
+const Blogs = () => {
+
+  const {data,mutate,error,isLoading}= useSWR('/api/posts/all', fetcher)
+  
   return (
     <div className={styles.container}>
       <div className={styles.headingContent}>
@@ -26,7 +32,7 @@ const Blogs = async() => {
       </div>
       <div className={styles.blogs}>
         {
-          data.map((item)=>(
+         data ? data.map((item)=>(
           <Link href={`/blogs/${item._id}`}>
             <div className={styles.blog}>
               <h1 className={styles.blogTitle}>{item.title}</h1>
@@ -34,11 +40,28 @@ const Blogs = async() => {
               <span className={styles.blogAuthor}>{item.username}</span>
             </div>
         </Link>
-          ))
+          )) : " "
         }
       </div>
     </div>
   )
 }
+
+export async function getServerSideProps(ctx) {
+  // get the current environment
+
+  // request posts from api
+  let response = await fetch(`${process.env.DEV_URL}/api/posts/all`);
+  // extract the data
+  let data = await response.json();
+
+  console.log(data)
+  return {
+      props: {
+          posts:data
+      },
+  };
+}
+
 
 export default Blogs
