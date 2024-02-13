81df3a462fe77b683575683b169e4fedc117ad33
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-02-13 19:33:46 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,35 @@
+/* eslint-disable */
 import Image from 'next/image'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button'
-import HeroImg from "public/HomePageHero.jpg";
+import HeroImg from "public/HomePageHero.png";
+import Blogs from './blogs/page';
 
 export default function Home() {
 
-if(!process.env.NEXT_PUBLIC_BASEURL){
-
-    console.log("null")
-    return null
-  }
-
   
   return (
+
+<>
+
     <div className={styles.container}>
       <div className={styles.textContent}>
-        <h1 className={styles.title}>A place to express your ideas!</h1>
-        <p className={styles.desc}>Building a community where we can talk about technology, future and ideas</p>
+        <h1 className={styles.title}>Start your blogging journey from here</h1>
+        <p className={styles.desc}>A perfect place to share ideas on technology</p>
         <Button url="/blogs" text="Read Blogs"/>
       </div>
       <div className={styles.imageContainer}>
         <Image src={HeroImg} alt="Coder Image" className={styles.img}/>
       </div>
     </div>
+<div>
+
+
+      <Blogs/>
+</div>
+
+</>
   )
 }
+
+
